7b858ffbc1beaaa4ce2aed6956706b49669b7f64
Thomas Boyt <me@thomasboyt.com>
2013-05-20 16:28:54 -0400

    allow gunicorn to do its thing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from comics import app
 import os
 
-DEBUG = os.getenv('COMICS_DEBUG', False)
-# if COMICS_DEBUG is set to anything, == True
-app.run(debug=bool(DEBUG))
+if __name__ == "__main__":
+    DEBUG = os.getenv('COMICS_DEBUG', False)
+    # if COMICS_DEBUG is set to anything, == True
+    app.run(debug=bool(DEBUG))
