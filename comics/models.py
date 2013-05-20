3f27a0076fb2cdbca2426f204934c60372f2f397
Thomas Boyt <me@thomasboyt.com>
2013-05-20 14:41:23 -0400

    Allow series->users backref, fix issue-title assoc

--- comics/models.py
+++ comics/models.py
@@ -25,15 +25,15 @@ class Issue(db.Model):
     release_date = db.Column(db.DateTime)
     title_id = db.Column(db.Integer, db.ForeignKey('title.id'))
 
-    def __init__(self, number, release_date, issue):
+    def __init__(self, number, release_date, title):
         self.number = number
         self.release_date = release_date
-        self.issue = issue
+        self.title = title
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Text)
-    titles = db.relationship('Title', secondary=titles_users)
+    titles = db.relationship('Title', secondary=titles_users, backref='users')
 
     def __init__(self, email, titles):
         self.email = email
