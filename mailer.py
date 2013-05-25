144eb50f8dfee5452a3f7f6dbafeca41c54b377a
Thomas Boyt <me@thomasboyt.com>
2013-05-25 14:18:24 -0400

    Added debug output to mailer

--- mailer.py
+++ mailer.py
@@ -11,6 +11,7 @@ from comics.models import Title, Issue, User
 
 MAILGUN_API_KEY = os.environ['MAILGUN_API_KEY']
 MAILGUN_DOMAIN = os.environ['MAILGUN_DOMAIN']
+DEBUG = os.getenv('DEBUG', False)
 
 env = Environment(loader=PackageLoader('comics', 'templates'))
 html_view = env.get_template("notification.html")
@@ -19,7 +20,6 @@ txt_view = env.get_template("notification.txt")
 def query_date(date):
     issues = Issue.query.filter_by(release_date=date).all()
 
-    # lol
     user_cache = defaultdict(lambda : defaultdict(list))
 
     for issue in issues:
@@ -36,17 +36,21 @@ def create_mail(users):
     for user, comics in users.iteritems():
         txt = txt_view.render(comics=comics)
         html = html_view.render(comics=comics)
-        r = requests.post(
-            url="https://api.mailgun.net/v2/%s/messages" % (MAILGUN_DOMAIN),
-            data={
-                "from": "Comics Notifier <notifier@comicsnotifier.mailgun.org>",
-                "to": user,
-                "subject": "New comics releases today",
-                "text": txt,
-                "html": html,
-            },
-            auth=auth
-        )
+        if not debug:
+            r = requests.post(
+                url="https://api.mailgun.net/v2/%s/messages" % (MAILGUN_DOMAIN),
+                data={
+                    "from": "Comics Notifier <notifier@comicsnotifier.mailgun.org>",
+                    "to": user,
+                    "subject": "New comics releases today",
+                    "text": txt,
+                    "html": html,
+                },
+                auth=auth
+            )
+        else:
+            print user
+            print txt
 
 if __name__ == "__main__":
     date = datetime(2013, 5, 22)
